a8027b47fe435f6625fd1e50dfbbf1eb77be6525
Roman Titkov <titkov01@mail.ru>
2024-06-10 13:03:57 +0300

    Update CLIP evaluator

--- evaluate_clip.py
+++ evaluate_clip.py
@@ -9,24 +9,26 @@ from PIL import Image, ImageDraw, ImageFont
 from tqdm import tqdm
 from transformers import CLIPProcessor, CLIPModel
 
+from collections import defaultdict
+from sklearn.metrics import precision_recall_fscore_support
 
 def draw_material(image_path, result, output_path):
     image = Image.open(image_path)
     draw = ImageDraw.Draw(image)
     try:
-        font = ImageFont.truetype("arial.ttf", size=20)
+        font = ImageFont.truetype("arial.ttf", size=40)
     except IOError:
-        font = ImageFont.load_default()
+        font = ImageFont.load_default(size=20)
 
     text = f"{result['material']} ({result['confidence']:.2f})"
-    draw.text((10, 10), text, font=font, fill=(255, 255, 255))
+    draw.text((10, 10), text, font=font, fill=(255, 0, 0))
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path))
     image.save(output_path)
 
 
 def evaluate_clip_model(model_path, dataset_root, material_markup, material_classes, debug=False):
-    output_markup = os.path.join(model_path, 'evaluate_stats_v2_withopenai.json')
+    output_markup = os.path.join(model_path, 'evaluate_clip_v2.json')
     if os.path.exists(output_markup):
         print(f'Markup already exists: {output_markup}')
         return
@@ -58,9 +60,31 @@ def evaluate_clip_model(model_path, dataset_root, material_markup, material_clas
     with open(material_markup, 'r') as file:
         material_references = json.load(file)
 
-    for image_path in tqdm(material_references):
+    # total_count = defaultdict(int)
+    # correct_count = defaultdict(int)
+    true_positives = defaultdict(int)
+    predicted_positives = defaultdict(int)
+    actual_positives = defaultdict(int)
+
+    dict_predictions = defaultdict(list)
+    dict_labels = defaultdict(list)
+
+    for material in material_classes:
+        true_positives[material] = 0
+        predicted_positives[material] = 0
+        actual_positives[material] = 0
+
+    import random
+    random.seed(10)
+    material_references_keys = list(material_references.keys())
+    random.shuffle(material_references_keys)
+    for idx, image_path in enumerate(tqdm(material_references_keys)):
+        # if idx == 100:
+        #     break
         object_name = ','.join(material_references[image_path]["name"]).strip()
         prompts = [f"The {object_name} is made of {material}" for material in material_classes]
+        # prompts = [f'a photo of a {material} {object_name}' for material in material_classes]
+        # prompts = [f'the {material} {object_name}' for material in material_classes]
         image_full_path = os.path.join(dataset_root, image_path)
 
         image = Image.open(image_full_path).convert("RGB")
@@ -71,9 +95,10 @@ def evaluate_clip_model(model_path, dataset_root, material_markup, material_clas
         probs = logits_per_image.softmax(dim=1)
         max_idx = probs.argmax().item()
         confidence = probs.max().item()
-        best_class = material_classes[max_idx]
+        confidence = probs.max().item()
+        predicted_class = material_classes[max_idx]
 
-        result = {"material": best_class, "confidence": confidence, "prompt": prompts[max_idx]}
+        result = {"material": predicted_class, "confidence": confidence, "prompt": prompts[max_idx]}
         result["all_materials"] = {cls: float(prob) for cls, prob in zip(material_classes, probs[0])}
         
         material_references[image_path]['predicted_material'] = result['material']
@@ -90,9 +115,37 @@ def evaluate_clip_model(model_path, dataset_root, material_markup, material_clas
             correct_count += 1
         
         material_references[image_path]['is_correct'] = is_correct
+
+        true_labels = material_references[image_path]['material']
+        for material in material_classes:
+            dict_labels[material].append(material in true_labels)
+            dict_predictions[material].append(material == predicted_class)
     
     accuracy = round(correct_count / total_count if total_count > 0 else 0, 2)
     print("Accuracy:", accuracy)
+
+    precision = {}
+    recall = {}
+    f1_scores = {}
+    for material in material_classes:
+        precision[material], recall[material], f1_scores[material], _ = precision_recall_fscore_support(
+            dict_labels[material],
+            dict_predictions[material],
+            average='binary')
+
+    mean_precision = 0
+    mean_recall = 0
+    mean_f1 = 0
+    for material in material_classes:
+        mean_precision += precision[material]
+        mean_recall += recall[material]
+        mean_f1 += f1_scores[material]
+        print(f"{material}: precision {precision[material]:.2f}, recall {recall[material]:.2f}, f1 {f1_scores[material]:.2f}, support {sum(dict_labels[material])}")
+
+    print(f'Mean precision: {round(mean_precision / len(material_classes), 2)}')
+    print(f'Mean recall: {round(mean_recall / len(material_classes), 2)}')
+    print(f'Mean f1: {round(mean_f1 / len(material_classes), 2)}')
+        
 
     material_references['info'] = {
         'model': model_path,
@@ -101,13 +154,13 @@ def evaluate_clip_model(model_path, dataset_root, material_markup, material_clas
 
     with open(output_markup, 'w') as f:
         json.dump(material_references, f, indent=4)
-
+    print(f'Results saved to {output_markup}')
 
 def main():
     parser = argparse.ArgumentParser(description="Evaluate CLIP model for material classification.")
     parser.add_argument("--model-path", type=str, help="Path to the pre-trained CLIP model or checkpoint.")
     parser.add_argument("--dataset-root", type=str, default="./", help="Root directory of the dataset.")
-    parser.add_argument("--material-markup", type=str, default="material_dataset_135_scenes/test_data.json", help="Path to the material markup file.")
+    parser.add_argument("--material-markup", type=str, default="material_dataset_135_scenes_v2/test_data.json", help="Path to the material markup file.")
     parser.add_argument("--models-dir", type=str, help="Path to the directory with models to test")
     parser.add_argument("--debug", action="store_true", help="Enable debug mode for more verbose output.")
 
